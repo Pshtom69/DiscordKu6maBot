b3f69e1f815fba4b62430ebb5f73afcc91bfb515
pshtomgit <72644222+pshtomgit@users.noreply.github.com>
2020-10-28 13:47:59 +0800

    modify for ku6ma

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const moment = require("moment");
 require("moment-duration-format");
 const momenttz = require('moment-timezone');
 const MessageAdd = require('./db/message_add.js')
-const welcomeChannelName = "안녕하세요";
+const welcomeChannelName = [
+  "안녕하세요",
+  "welcome",
+  "welcome&bye",
+]
 const byeChannelName = "안녕히가세요";
 const welcomeChannelComment = "어서오세요.";
 const byeChannelComment = "안녕히가세요.";
